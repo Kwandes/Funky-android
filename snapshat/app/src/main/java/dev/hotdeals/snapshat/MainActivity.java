b4042cee4f664d53e20394a102fc32823773a0fe
Kwandes <bz.jasiek@gmail.com>
2021-05-26 23:00:57 +0200

    initialise the android app

---
+++ snapshat/app/src/main/java/dev/hotdeals/snapshat/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.hotdeals.snapshat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
