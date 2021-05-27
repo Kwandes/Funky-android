b9121f736b071509bae4e8428113f8a97d4334cb
Kwades <bz.jasiek@gmail.com>
2021-05-27 23:31:46 +0200

    Implement Firestore

--- snapshat/app/src/main/java/dev/hotdeals/snapshat/MainActivity.java
+++ snapshat/app/src/main/java/dev/hotdeals/snapshat/MainActivity.java
@@ -1,14 +1,67 @@
 package dev.hotdeals.snapshat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    TextView helloWorldTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloWorldTxt = findViewById(R.id.helloWorldTxt);
+
+    }
+
+    public void testFirestore(View view)
+    {
+        Map<String, Object> dataToSave = new HashMap<>();
+        dataToSave.put("test part one", "test stuff");
+        dataToSave.put("test part two", "more test stuff");
+        db.document("test/example").set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("Saving result", "Thign has been saved");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("Saving result", e);
+            }
+        });
+        db.collection("test").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("Yeet", document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.w("Yeet", "Error getting documents.", task.getException());
+                }
+            }
+        });
     }
 }
